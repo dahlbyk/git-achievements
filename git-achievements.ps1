a6c7e9718283021a6673b256722e3ec37ec6e528
Cori Drew <CoriDrew@gmail.com>
2012-05-17 23:55:50 -0500

    modified method of getting path to sh.exe to fix bug when not installed in default path location

--- git-achievements.ps1
+++ git-achievements.ps1
@@ -6,8 +6,8 @@ $gaDir = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent
 $gitX = [Environment]::GetEnvironmentVariable("gitAchievementsGitPath", "User");
 if(! $gitX)
 {
-	$gitEx = 'Git\bin\sh.exe';
-	$gitX = $Env:ProgramFiles, ${Env:ProgramFiles(x86)}, $Env:ProgramW6432 | %{ "$_\$gitEx" } | ?{ Test-Path $_ } | select -first 1
+	
+	$gitX = (get-command sh).definition
 	[Environment]::SetEnvironmentVariable("gitAchievementsGitPath", $gitX, "User")
 }
 
